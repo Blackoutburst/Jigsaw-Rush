114f9bba4619b0976f9455b060f11f4d5eb2cabc
Blackoutburst <blackoutburst@hotmail.com>
2022-03-02 19:44:00 +0100

    Cancelling events

--- src/com/blackoutburst/pgjigsaw/main/Main.java
+++ src/com/blackoutburst/pgjigsaw/main/Main.java
@@ -1,4 +1,52 @@
 package com.blackoutburst.pgjigsaw.main;
 
-public class Main {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main  extends JavaPlugin implements Listener {
+
+    public static boolean gameRunning = false;
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
 }
